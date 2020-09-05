b8b6911b8569515ed8ab706fea115e10a718cf03
themarkyone <markyin2001@gmail.com>
2020-09-05 18:34:53 -0400

    finished cards on the second page

--- squad.js
+++ squad.js
@@ -1,4 +1,8 @@
-const squadCards = [
+const squad = Handlebars.compile(
+    document.querySelector('#squadTemplate').innerHTML
+);
+
+const squadInfos = [
     {
         number: "1", 
         name: "Manuel Neuer",
@@ -6,7 +10,7 @@ const squadCards = [
         position: "Goalkeeper",
         nation: "German",
         pic:"https://i.pinimg.com/736x/8a/e9/1d/8ae91de111eb1c5afb8c1da83839384d.jpg",
-        row_num: "1",
+        rowNum: "1",
     },
     {
         number: "6", 
@@ -15,7 +19,7 @@ const squadCards = [
         position: "Central Midfielder",
         nation: "Spainish",
         pic:"https://www.thesun.co.uk/wp-content/uploads/2020/08/NINTCHDBPICT000598863010.jpg",
-        row_num: "1",
+        rowNum: "1",
     },
     {
         number: "9", 
@@ -24,7 +28,7 @@ const squadCards = [
         position: "Striker",
         nation: "Polish",
         pic:"https://img.fcbayern.com/image/fetch/f_auto,h_880,q_auto:good,w_660/https://fcbayern.com/binaries/content/gallery/fc-bayern/homepage/saison-19-20/galerien/spiele/fcb-union-berlin/13_fcb-union_191026_get.jpg/13_fcb-union_191026_get.jpg/fcbhippo%3Alargeninetotwelve%3Fv%3D1572101462955",
-        row_num: "1",
+        rowNum: "1",
     },
     {
         number: "19", 
@@ -33,7 +37,7 @@ const squadCards = [
         position: "Left Back",
         nation: "Canadian",
         pic:"https://img.fcbayern.com/image/fetch/f_auto,h_880,q_auto:good,w_660/https://fcbayern.com/binaries/content/gallery/fc-bayern/homepage/saison-19-20/profis/davies/191211_davies_ima.jpg/191211_davies_ima.jpg/fcbhippo%3Alargeninetotwelve%3Fv%3D1578476315185",
-        row_num: "1",
+        rowNum: "1",
     },
     {
         number: "22", 
@@ -42,7 +46,7 @@ const squadCards = [
         position: "Right Winger",
         nation: "German",
         pic:"https://img.fcbayern.com/image/fetch/f_auto,h_880,q_auto:good,w_660/https://fcbayern.com/binaries/content/gallery/fc-bayern/homepage/saison-19-20/profis/gnabry/200613_gnabry_get.jpg/200613_gnabry_get.jpg/fcbhippo%3Alargeninetotwelve%3Fv%3D1594727321666",
-        row_num: "2",
+        rowNum: "2",
     },
     {
         number: "25", 
@@ -51,7 +55,7 @@ const squadCards = [
         position: "Striker",
         nation: "German",
         pic:"https://staticg.sportskeeda.com/editor/2020/02/69e3a-15827383259800.jpg",
-        row_num: "2",
+        rowNum: "2",
     },
     {
         number: "32", 
@@ -60,7 +64,7 @@ const squadCards = [
         position: "Right Back",
         nation: "German",
         pic:"https://img.fcbayern.com/image/fetch/f_auto,h_880,q_auto:good,w_660/https://fcbayern.com/binaries/content/gallery/fc-bayern/homepage/saison-19-20/profis/kimmich/200303_kimmich_ima.jpg/200303_kimmich_ima.jpg/fcbhippo%3Alargeninetotwelve%3Fv%3D1583276610979",
-        row_num: "2",
+        rowNum: "2",
     },
     {
         number: "#", 
@@ -69,6 +73,28 @@ const squadCards = [
         position: "Coach",
         nation: "German",
         pic:"https://img.fcbayern.com/image/fetch/f_auto,h_880,q_auto:good,w_660/https://fcbayern.com/binaries/content/gallery/fc-bayern/homepage/saison-19-20/trainer/flick/191127_flick_ima.jpg/191127_flick_ima.jpg/fcbhippo%3Alargeninetotwelve%3Fv%3D1574849933575",
-        row_num: "2",
+        rowNum: "2",
     },
 ]
+
+for (let m of squadInfos) {
+    if (m.rowNum === "1") {
+        document.querySelector('#row1').innerHTML += squad({
+            number: m.number,
+            name: m.name,
+            birth: m.birth,
+            position: m.position,
+            nation: m.nation,
+            pic: m.pic,
+        })
+    } else {
+        document.querySelector('#row2').innerHTML += squad({
+            number: m.number,
+            name: m.name,
+            birth: m.birth,
+            position: m.position,
+            nation: m.nation,
+            pic: m.pic,
+        })
+    }
+}
